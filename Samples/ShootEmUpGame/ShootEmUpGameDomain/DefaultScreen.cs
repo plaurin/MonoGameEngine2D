d1832b99aa61894706a12a84468432f1918e4b90
Pascal Laurin <plaurin@hotmail.com>
2012-10-08 12:29:31 -0400

    Sample: Added ShootEmUp sample

---
+++ Samples/ShootEmUpGame/ShootEmUpGameDomain/DefaultScreen.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using GameFramework;
+using GameFramework.Cameras;
+using GameFramework.Inputs;
+using GameFramework.Scenes;
+using GameFramework.Screens;
+
+namespace ShootEmUpGameDomain
+{
+    public class DefaultScreen : ScreenBase
+    {
+        private Camera camera;
+
+        private InputConfiguration inputConfiguration;
+
+        private GameResourceManager gameResourceManager;
+
+        private Scene scene;
+
+        public override void Initialize(Camera theCamera)
+        {
+            this.camera = theCamera;
+
+            this.camera.Center = CameraCenter.WindowTopLeft;
+        }
+
+        public override InputConfiguration GetInputConfiguration()
+        {
+            this.inputConfiguration = new InputConfiguration();
+
+            return this.inputConfiguration;
+        }
+
+        public override void LoadContent(GameResourceManager resourceManager)
+        {
+            this.gameResourceManager = resourceManager;
+        }
+
+        public override void Update(double elapsedSeconds, int fps)
+        {
+        }
+
+        public override Scene GetScene()
+        {
+            this.scene = new Scene("MainScene");
+
+            return this.scene;
+        }
+    }
+}
---
+++ Samples/ShootEmUpGame/XnaShootEmUpGame/Game.cs
@@ -0,0 +1,19 @@
+using System;
+
+using ShootEmUpGameDomain;
+
+using XnaGameFramework;
+
+namespace XnaShootEmUpGame
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game : WindowsGameBase
+    {
+        public Game()
+            : base(new DefaultScreen())
+        {
+        }
+    }
+}
