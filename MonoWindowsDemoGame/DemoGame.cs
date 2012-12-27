9815054981cc541d4af11001857c79a4e1d16f24
Pascal Laurin <pascal.laurin@hotmail.com>
2012-12-27 15:34:23 -0500

    Added MonoGameFramework and Demo project

---
+++ MonoWindowsDemoGame/DemoGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DemoGameDomain;
+using XnaGameFramework;
+
+namespace MonoWindowsDemoGame
+{
+    public class DemoGame : WindowsGameBase
+    {
+        public DemoGame()
+            : base(new DefaultScreen())
+        {
+            IsMouseVisible = true;
+        }
+    }
+}
---
+++ MonoWindowsDemoGame/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace MonoWindowsDemoGame
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        private static DemoGame game;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            game = new DemoGame();
+            game.Run();
+        }
+    }
+}
