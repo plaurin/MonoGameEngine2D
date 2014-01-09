e069d026c6d6568fec35756f4f6e9695de427063
Pascal Laurin <plaurin@hotmail.com>
2014-01-08 22:11:28 -0500

    First version of ContentSync

--- ContentSync/Program.cs
+++ ContentSync/Program.cs
@@ -1,15 +1,185 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ContentSync
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        // Should always be Content?
+        private const string ContentFolder = @"Content\";
+        private static readonly XNamespace Xmlns = "http://schemas.microsoft.com/developer/msbuild/2003";
+
+        public static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("ContentSync: ");
+
+                if (args.Length != 2)
+                {
+                    Console.WriteLine("Expected arguments source and destination projects file path");
+                    return;
+                }
+
+                var source = args[0];
+                var destination = args[1];
+
+                if (!File.Exists(source))
+                {
+                    Console.WriteLine("Source project file not found: " + source);
+                    return;
+                }
+
+                if (!File.Exists(destination))
+                {
+                    Console.WriteLine("Destination project file not found: " + destination);
+                    return;
+                }
+
+                Console.WriteLine(source + " to " + destination);
+
+                var destinationDocument = XDocument.Load(destination);
+
+                var sourceContentFiles = GetContentFiles(source).ToList();
+                var destinationContentFiles = GetLinkedContentFiles(destinationDocument).ToList();
+
+                var relativePathFromSourceToDestination = GetRelativePathFromSourceToDestination(source, destination);
+
+                var hasChanges = AddMissingContents(sourceContentFiles, destinationContentFiles, destinationDocument, relativePathFromSourceToDestination);
+
+                hasChanges |= RemoveExtraContent(sourceContentFiles, destinationContentFiles, destinationDocument);
+
+                // Check conformity of existing content
+
+                if (hasChanges)
+                {
+                    Console.WriteLine("Saving the updated project file " + destination);
+                    //destinationDocument.Save(destination);
+                }
+                else
+                {
+                    Console.WriteLine("No changes detected");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public static IEnumerable<XElement> GetContentFiles(string projectPath)
+        {
+            var projectDocument = XDocument.Load(projectPath);
+
+            return GetContentFiles(projectDocument);
+        }
+
+        public static IEnumerable<XElement> GetLinkedContentFiles(string projectPath)
+        {
+            var projectDocument = XDocument.Load(projectPath);
+
+            return GetLinkedContentFiles(projectDocument);
+        }
+
+        public static IEnumerable<XElement> GetLinkedContentFiles(XDocument projectDocument)
+        {
+            return projectDocument.Descendants(Xmlns + "Content")
+                .Where(x => GetLinkPath(x).StartsWith(ContentFolder));
+        }
+
+        public static string GetRelativePathFromSourceToDestination(string sourceProjectPath,
+            string destinationProjectPath)
+        {
+            var sourceProjectContentFolder = Path.GetDirectoryName(sourceProjectPath);
+            var destinationProjectContentFolder = Path.GetDirectoryName(destinationProjectPath);
+
+            var file = new Uri(sourceProjectContentFolder);
+
+            // Must end in a slash to indicate folder
+            var folder = new Uri(destinationProjectContentFolder + Path.DirectorySeparatorChar);
+
+            return Uri.UnescapeDataString(folder.MakeRelativeUri(file).ToString().Replace('/', Path.DirectorySeparatorChar));
+        }
+
+        public static bool AddMissingContents(List<XElement> sourceContentFiles, List<XElement> destinationContentFiles,
+            XDocument destinationDocument, string relativePathFromSourceToDestination)
+        {
+            var hasChanges = false;
+
+            var missingContentFiles = sourceContentFiles.Select(GetContentPath)
+                .Except(destinationContentFiles.Select(GetLinkPath))
+                .ToList();
+
+            if (missingContentFiles.Any())
+            {
+                hasChanges = true;
+                //        <Content Include="..\SamplesBrowser\Content\Sandbox\HexSheet.png">
+                //          <Link>Content\Sandbox\HexSheet.png</Link>
+                //          <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+                //        </Content>
+
+                foreach (var missingContentFile in missingContentFiles)
+                {
+                    Console.WriteLine("Adding missing content file " + missingContentFile);
+                    destinationDocument.Root.Add(
+                        new XElement(Xmlns + "ItemGroup",
+                            new XElement(Xmlns + "Content",
+                                new XAttribute("Include", Path.Combine(relativePathFromSourceToDestination, missingContentFile)),
+                                new XElement(Xmlns + "Link", missingContentFile),
+                                new XElement(Xmlns + "CopyToOutputDirectory", "PreserveNewest"))));
+                }
+            }
+
+            return hasChanges;
+        }
+
+        public static bool RemoveExtraContent(List<XElement> sourceContentFiles, List<XElement> destinationContentFiles,
+            XDocument destinationDocument)
+        {
+            var hasChanges = false;
+
+            var extraContentFiles = destinationContentFiles.Select(GetLinkPath)
+                .Except(sourceContentFiles.Select(GetContentPath))
+                .ToList();
+
+            if (extraContentFiles.Any())
+            {
+                hasChanges = true;
+
+                foreach (var extraContentFile in extraContentFiles)
+                {
+                    Console.WriteLine("Remove extra content file " + extraContentFile);
+
+                    var extraElement = destinationDocument.Descendants(Xmlns + "Link").Single(x => x.Value == extraContentFile).Parent;
+
+                    extraElement.Remove();
+                }
+            }
+
+            return hasChanges;
+        }
+
+        private static IEnumerable<XElement> GetContentFiles(XDocument projectDocument)
+        {
+            return projectDocument.Descendants(Xmlns + "Content")
+                .Where(x => GetContentPath(x).StartsWith(ContentFolder));
+        }
+
+        private static string GetContentPath(XElement contentElement)
+        {
+            return contentElement.Attribute("Include") != null
+                ? contentElement.Attribute("Include").Value
+                : string.Empty;
+        }
+
+        private static string GetLinkPath(XElement contentElement)
+        {
+            return contentElement.Element(Xmlns + "Link") != null
+                ? contentElement.Element(Xmlns + "Link").Value
+                : string.Empty;
         }
     }
 }
---
+++ ContentSyncTests/ProgramTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using ContentSync;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContentSyncTests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        private const string Source = @"C:\Users\Pascal\Dev\DotNet\GitHub\XNAGameEngine2D\SamplesBrowser\SamplesBrowser.csproj";
+        private const string Destination = @"C:\Users\Pascal\Dev\DotNet\GitHub\XNAGameEngine2D\WindowsSamples\WindowsSamples.csproj";
+
+        private static readonly XNamespace Xmlns = "http://schemas.microsoft.com/developer/msbuild/2003";
+
+        [TestMethod]
+        public void TestGetContentFiles()
+        {
+            var contentFiles = Program.GetContentFiles(Source);
+
+            Assert.AreEqual(5, contentFiles.Count());
+        }
+
+        [TestMethod]
+        public void TestGetLinkedContentFiles()
+        {
+            var contentFiles = Program.GetLinkedContentFiles(Destination);
+
+            Assert.AreEqual(5, contentFiles.Count());
+        }
+
+        [TestMethod]
+        public void GetRelativePathFromSourceToDestination()
+        {
+            var result = Program.GetRelativePathFromSourceToDestination(Source, Destination);
+
+            Assert.AreEqual(@"..\SamplesBrowser", result);
+        }
+
+        [TestMethod]
+        public void WhenMissingContentShouldAddLinks()
+        {
+            var sourceContentFiles = Program.GetContentFiles(Source).ToList();
+            var destinationContentFiles = Program.GetLinkedContentFiles(Destination).ToList();
+            var destinationDocument = XDocument.Load(Destination);
+            var relativePath = Program.GetRelativePathFromSourceToDestination(Source, Destination);
+
+            sourceContentFiles.Add(new XElement(Xmlns + "Content",
+                new XAttribute("Include", @"Content\New1.png")));
+
+            Program.AddMissingContents(sourceContentFiles, destinationContentFiles, destinationDocument, relativePath);
+
+            var newDestinationContentFiles = destinationDocument.Descendants(Xmlns + "Content")
+                .Where(x => x.Element(Xmlns + "Link") != null && x.Element(Xmlns + "Link").Value.StartsWith(@"Content\")).ToList();
+
+            Assert.AreEqual(destinationContentFiles.Count() + 1, newDestinationContentFiles.Count());
+
+            var newElement = newDestinationContentFiles.Single(x => x.Element(Xmlns + "Link").Value.Contains("New1.png"));
+
+            const string Expected = @"<Content Include=""..\SamplesBrowser\Content\New1.png"" xmlns=""http://schemas.microsoft.com/developer/msbuild/2003""><Link>Content\New1.png</Link><CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory></Content>";
+
+            Assert.AreEqual(Expected, newElement.ToString(SaveOptions.OmitDuplicateNamespaces | SaveOptions.DisableFormatting));
+        }
+
+        [TestMethod]
+        public void WhenExtraContentShouldRemoveLinks()
+        {
+            var sourceContentFiles = Program.GetContentFiles(Source).ToList();
+            var destinationDocument = XDocument.Load(Destination);
+
+            destinationDocument.Root.Add(
+                new XElement(Xmlns + "ItemGroup",
+                    new XElement(Xmlns + "Content",
+                        new XAttribute("Include", @"..\SamplesBrowser\Content\Old.png"),
+                        new XElement(Xmlns + "Link", @"Content\Old1.png"))));
+
+            var destinationContentFiles = Program.GetLinkedContentFiles(destinationDocument).ToList();
+
+            Program.RemoveExtraContent(sourceContentFiles, destinationContentFiles, destinationDocument);
+
+            var newDestinationContentFiles = destinationDocument.Descendants(Xmlns + "Content")
+                .Where(x => x.Element(Xmlns + "Link") != null && x.Element(Xmlns + "Link").Value.StartsWith(@"Content\")).ToList();
+
+            Assert.AreEqual(destinationContentFiles.Count() - 1, newDestinationContentFiles.Count());
+        }
+
+        [TestMethod]
+        public void TempTestMain()
+        {
+            Program.Main(new[] { Source, Destination });
+        }
+    }
+}
