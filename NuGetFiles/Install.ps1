e5a1240d0ece4fd3c8028cad2d96ef9ee7ccfccc
Pascal Laurin <plaurin@hotmail.com>
2014-03-08 15:09:52 -0500

    Added ContentSync to GameLogic package

--- NuGetFiles/Install.ps1
+++ NuGetFiles/Install.ps1
@@ -1,9 +1,10 @@
 param($installPath, $toolsPath, $package, $project)
 
-# $project.ProjectItems | select-Object Name | Write-Host
+# SpriteFont.xnb build action
 $item = $project.ProjectItems | where-object {$_.Name -eq "Content"} 
-
-# $item.ProjectItems | select-Object Name | Write-Host
 $item = $item.ProjectItems | where-object {$_.Name -eq "SpriteFont.xnb"} 
-
 $item.Properties.Item("BuildAction").Value = [int]2
+
+# Content Sync
+$pre = $project.Properties | where-object {$_.Name -eq "PreBuildEvent"}
+$pre.Value = 'REM Replace WindowsProject by your actual project name: "$(ProjectDir)\SyncTool\ContentSync.exe" "$(ProjectPath)" "$(SolutionDir)\WindowsProject\WindowsProjects.csproj"'
